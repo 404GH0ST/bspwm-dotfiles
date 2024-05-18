a952446d882683f399ef8c5949a89648f969e05b
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-05-18 16:02:24 +0700

    chore: adjust grub script

--- scripts/install_grub_theme.sh
+++ scripts/install_grub_theme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo cp -R $HOME/bspwm-dotfiles/misc/grub/catppuccin-mocha-grub-theme /usr/share/grub/themes
+sudo tar xzf $HOME/bspwm-dotfiles/misc/grub/catppuccin-mocha-grub-theme.tar.gz --directory /usr/share/grub/themes
 sudo sed -i 's|#GRUB_THEME=".*"|GRUB_THEME="/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt"|' /etc/default/grub
 sudo sed -i '/^#.*OS_PROBER=\w/s/^#//' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
